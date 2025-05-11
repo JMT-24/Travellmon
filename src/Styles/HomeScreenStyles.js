de4e28f8817a7d871c18000c63c180d86abdfdea
JMT <tjuanmiguel116@gmail.com>
2025-05-11 15:29:09 +0800

    will try implementing get locations

---
+++ src/Styles/HomeScreenStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    body: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    container: {
+        //backgroundColor: "blue",
+        width: "90%",
+        height: "90%",
+    },
+    appText: {
+        color: "black",
+        fontSize: 20,
+    },
+});
+
+export default styles;
